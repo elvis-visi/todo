b5143eb0564abf46986bcda0bd90fe775ecf2542
elvis-visi <sivisevi1@gmail.com>
2024-03-21 10:32:08 +0100

    Added docker-compose with volumes, binding a file from host machine to container

--- todo-app/todo-backend/mongo/index.js
+++ todo-app/todo-backend/mongo/index.js
@@ -1,8 +1,14 @@
 const mongoose = require('mongoose')
 const Todo = require('./models/Todo')
-const { MONGO_URL } = require('../util/config')
+// const { MONGO_URL } = require('../util/config')
 
-if (MONGO_URL && !mongoose.connection.readyState) mongoose.connect(MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://root:example@localhost:3456/the_database', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 30000, // Increase the timeout to 30 seconds
+}).catch((error) => {
+  console.error('Error connecting to MongoDB:', error);
+});
 
 
 module.exports = {
